7d19bc86c34fa96432db977eabb3f40bfe1fc9f4
Brandon Evans <brandon@brandonevans.ca>
2020-01-08 21:24:13 -0700

    Release highlights-to-markdown 1.1.1

--- Formula/highlights-to-markdown.rb
+++ Formula/highlights-to-markdown.rb
@@ -1,10 +1,10 @@
 class HighlightsToMarkdown < Formula
   desc "Extracts highlight annotations from a PDF file and prints them as Markdown quotes"
   homepage "https://github.com/interstateone/highlights-to-markdown"
-  url "https://github.com/interstateone/highlights-to-markdown.git", revision: "c0259c8c733a819954319b2aac57eab680abf4c1"
+  url "https://github.com/interstateone/highlights-to-markdown.git", tag: "1.1.1"
   head "https://github.com/interstateone/highlights-to-markdown.git"
 
-  depends_on xcode: ["10.0", :build]
+  depends_on xcode: ["11.0", :build]
 
   def install
     system "make", "install", "prefix=#{prefix}"
